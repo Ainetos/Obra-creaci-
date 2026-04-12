a221e21a864f70fb547d9214dfc799dbf40eaea5
Ainetos <airupim@gmail.com>
2026-04-12 13:54:31 +0200

    arreglillos ahora hay formitas

--- sketch.js
+++ sketch.js
@@ -3,18 +3,19 @@ const tiempoAdivinar = 5;
 let colorAleatorio = 0;
 let formaAleatorio = 0;
 let colorFalso = 0;
+let formaFalso = 0;
 let tiempo = 0;
 
 let rojo, azul, rosa, verde, naranja, lila;
 let colores = [];
-let coloresSTR = ["rojo", "azul", "rosa", "verde", "naranja", "lila"];
+let coloresSTR = ["Rojo", "Azul", "Rosa", "Verde", "Naranja", "Lila"];
 
-let formasSTR = ["rombo", "cuadrado", "rectángulo", "trapecio", "pentágono", "polígono estrellado"];
+let formasSTR = ["rombo", "cuadrado", "rectángulo", "trapecio", "círculo", "elipse"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  frameRate(1);
+  frameRate(3);
 
   rojo = color(255, 0, 0);
   azul = color(0, 0, 255);
@@ -26,31 +27,51 @@ function setup() {
   colores = [rojo, azul, rosa, verde, naranja, lila];
   controlFrames = 0;
   textSize(50);
-  tiempo = 6;
+  tiempo = 30;
 
-  ValoresAleatorios();
 }
 
 function draw() {
   background(220);
   fill(0);
+  rectMode(RADIUS);
   tiempo = tiempo - frameCount;
-  text(tiempo, 50, 50);
-  tiempo = 6;
+  textAlign(CENTER, CENTER);
+  text(int (tiempo/3), 50, 50);
+  tiempo = 30;
+
+   CambioRandom();
 
 
-  if((frameCount <= 2) && (frameCount > 0)){
+  if((frameCount <= 6) && (frameCount > 0)){
     fill(colores[colorFalso]);
+    textStyle(BOLD);
+    textSize(80)
     text(coloresSTR[colorAleatorio], (width/2) - 45, height/2);
     fill(220);
-  } else if(frameCount <= 0) {
+    textStyle(NORMAL);
+    textSize(50)
+  } 
+  else if((frameCount <= 15) && (frameCount > 6)){
+    fill(colores[colorFalso]);
+    dibujoForma(formasSTR[formaAleatorio], colorFalso);
+    text(formasSTR[formaAleatorio], 0, height/2);
+    fill(220);
+  }
+  else if(frameCount <= 0) {
     ValoresAleatorios();
   }
 
   fill(220);
   // print(frameCount);
-  if(frameCount > 5){
+  if(frameCount > 27){
     frameCount = 0;
+  }
+}
+
+function CambioRandom(){
+  if(frameCount <= 1){
+    ValoresAleatorios();
   }
 }
 
@@ -64,4 +85,25 @@ function ValoresAleatorios(){
   }
 
   print(colorAleatorio, formaAleatorio, colorFalso);
+}
+
+function dibujoForma(forma){
+  if(forma == "rombo"){
+     quad(windowWidth/2, windowHeight/4, windowWidth*3/4, windowHeight/2, windowWidth/2, windowHeight*3/4, windowWidth/4, windowHeight/2);
+  }
+  else if(forma == "cuadrado"){
+    square(windowWidth/2, windowHeight/2, windowHeight/3);
+  }
+  else if(forma == "rectángulo"){
+    rect(windowWidth/2, windowHeight/2, windowHeight/2, windowHeight/4);
+  }
+  else if(forma == "trapecio"){
+    quad(windowWidth/4, windowHeight/4, windowWidth*3/4, windowHeight/4, windowWidth*2/3, windowHeight*3/4, windowWidth*1/3, windowHeight*3/4);
+  }
+  else if(forma == "círculo"){
+    circle(windowWidth/2, windowHeight/2, windowHeight*3/4);
+  }
+  else if(forma == "elipse"){
+    ellipse(windowWidth/2, windowHeight/2, windowHeight, windowHeight/2);
+  }
 }
